b41414d24ca5e1f512f7c27ab0ac07ab3c5a9963
olita16 <119816415+olita16@users.noreply.github.com>
2025-11-28 07:33:48 +0000

    Rename lesson-5 to lesson-7

--- lesson-7/main.tf
+++ lesson-7/main.tf
@@ -1,6 +1,6 @@
 module "s3_backend" {
   source      = "./modules/s3-backend"
-  bucket_name = "lesson-5-terraform-state-826232761489" 
+  bucket_name = "lesson-7-terraform-state-826232761489" 
   table_name  = "terraform-locks"
 }
 
@@ -10,12 +10,12 @@ module "vpc" {
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   availability_zones = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-  vpc_name           = "lesson-5-vpc"
+  vpc_name           = "lesson-7-vpc"
 }
 
 
 module "ecr" {
   source       = "./modules/ecr"
-  ecr_name     = "lesson-5-ecr"
+  ecr_name     = "lesson-7-ecr"
   scan_on_push = true
 }
