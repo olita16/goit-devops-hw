147c1117faf0196567a402238f799d28d7e336f2
olita16 <119816415+olita16@users.noreply.github.com>
2025-11-30 15:48:17 +0000

    Delete lesson-7 folder

--- lesson-7/modules/s3-backend/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "Ім'я існуючого S3 бакету для Terraform state"
-  type        = string
-}
-
-variable "table_name" {
-  description = "Ім'я існуючої DynamoDB таблиці для блокування Terraform state"
-  type        = string
-}
